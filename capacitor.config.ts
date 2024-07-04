7bd113bd70c728a157419c1bf04dc103b076681c
Gabriel <bielgabrieloliveira77@gmail.com>
2024-07-04 13:18:23 -0300

    chore: Update npm dependencies and devDependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import type { CapacitorConfig } from "@capacitor/cli";
+
+const config: CapacitorConfig = {
+  appId: "com.go.health",
+  appName: "Go Health",
+  webDir: ".next",
+  bundledWebRuntime: false,
+};
+
+export default config;
